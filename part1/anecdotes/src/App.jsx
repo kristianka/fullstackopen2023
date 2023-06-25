014f4c1b727692f9826db25845db6f410f07a95a
Kristian <49764796+kristianka@users.noreply.github.com>
2023-06-25 17:08:37 +0300

    finish anecdotes

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -4,12 +4,40 @@ const getRand = () => {
   return Math.floor(Math.random() * 8);
 }
 
-const Entiia = (props) => {
-  const randNum = getRand();
-  console.log(randNum);
+const VoteQuote = (props) => {
+  const { points, selected, setPoints } = props;
+
+  const handleVoteClick = () => {
+    const updatedPoints = [...points];
+    updatedPoints[selected] += 1;
+    setPoints(updatedPoints);
+  };
+
   return (
     <div>
-      <button onClick={() => setSelected(getRand())}>Next</button>
+      <button onClick={handleVoteClick}>Vote +1</button>
+    </div>
+  )
+}
+
+const ChangeQuote = (props) => {
+  return (
+    <>
+      <button onClick={() => props.setSelected(getRand())}>Next anecdote</button>
+    </>
+  )
+}
+
+const DisplayMostVotes = (props) => {
+  const { anecdotes, points } = props;
+  const max = Math.max(...points);
+  const index = points.indexOf(max);
+
+  return (
+    <div>
+      <h2>Anecdote with the most votes</h2>
+      {anecdotes[index]}
+      <p>{max} votes</p>
     </div>
   )
 }
@@ -27,15 +55,17 @@ const App = () => {
   ]
 
   const [selected, setSelected] = useState(0);
-  const points = [0, 0, 0, 0, 0, 0, 0, 0];
-  const copy = [...points];
-
+  // fill an array with zeroes
+  const [points, setPoints] = useState(Array(8).fill(0));
+  console.log(points);
   return (
     <div>
+      <h1>Anecdote of the day</h1>
       {anecdotes[selected]}
-      <div>
-      </div>
-      <Entiia />
+      <p>has {points[selected]} votes</p>
+      <VoteQuote selected={selected} points={points} setPoints={setPoints} />
+      <ChangeQuote setSelected={setSelected} />
+      <DisplayMostVotes anecdotes={anecdotes} points={points} />
     </div>
   )
 }
