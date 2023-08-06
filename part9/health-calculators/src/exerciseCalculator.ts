760d6af88021bf24a645953317895fcbdf9e3c4e
Kristian <49764796+kristianka@users.noreply.github.com>
2023-08-06 20:41:05 +0300

    update to add argv

--- part9/health-calculators/src/exerciseCalculator.ts
+++ part9/health-calculators/src/exerciseCalculator.ts
@@ -1,5 +1,5 @@
 
-interface exercisesResult {
+interface ExercisesResult {
     periodLength: number,
     trainingDays: number,
     success: boolean,
@@ -9,14 +9,14 @@ interface exercisesResult {
     average: number
 }
 
-const calculateExercises = (exerciseHours: number[], target: number): exercisesResult => {
+const calculateExercises = (exerciseHours: number[], target: number): ExercisesResult => {
     const totalHours = exerciseHours.reduce((a, b) => a + b, 0);
     const avgHours = totalHours / exerciseHours.length;
     const totalDays = exerciseHours.length;
     const exercisedDays = exerciseHours.filter((n) => n !== 0).length;
 
     let succeeded = false;
-    if (totalDays >= target) {
+    if (exerciseHours.every((h) => h >= 2)) {
         succeeded = true;
     }
 
@@ -24,18 +24,22 @@ const calculateExercises = (exerciseHours: number[], target: number): exercisesR
     let rating = 0;
     let ratingDescription = "";
     const percentage: number = (totalHours / (target * 7)) * 100;
+    console.log(percentage)
     if (percentage <= 50) {
         rating = 1;
         ratingDescription = "You can do better!";
     } else if (percentage <= 75) {
         rating = 2;
         ratingDescription = "Good but little more to go!";
-    } else if (percentage >= 90) {
+    } else if (percentage <= 100) {
         rating = 3;
         ratingDescription = "Excellent!";
+    } else {
+        rating = 3;
+        ratingDescription = "Super Excellent!"
     }
 
-    const results: exercisesResult = {
+    const results: ExercisesResult = {
         periodLength: totalDays,
         trainingDays: exercisedDays,
         success: succeeded,
@@ -47,6 +51,44 @@ const calculateExercises = (exerciseHours: number[], target: number): exercisesR
     return results;
 }
 
-const dailyExerciseHours = [3, 0, 2, 4.5, 0, 3, 1];
-const dailyExerciseHourGoal = 2;
-console.log(calculateExercises(dailyExerciseHours, dailyExerciseHourGoal));
+// const dailyExerciseHours = [3, 0, 2, 4.5, 0, 3, 1];
+// const dailyExerciseHourGoal = 2;
+// console.log(calculateExercises(dailyExerciseHours, dailyExerciseHourGoal));
+
+interface ExerciseInput {
+    daysTarget: number,
+    exercisedHoursPerDay: number[]
+};
+
+const parseArguments = (args: string[]): ExerciseInput => {
+    if (args.length < 5) throw new Error("Not enough arguments");
+
+    // remove unnecessary stuff, target and check that every input is a number
+    const userInput = args.slice(3);
+    userInput.forEach(element => {
+        if (isNaN(Number(element))) {
+            throw new Error("Every value must be a number!")
+        }
+    });
+
+    // remove target from array
+    const hours: number[] = userInput.map(Number);
+    console.log("hours", hours)
+    return {
+        exercisedHoursPerDay: hours,
+        daysTarget: Number(args[2])
+    }
+}
+
+try {
+    const { exercisedHoursPerDay, daysTarget } = parseArguments(process.argv);
+    console.log(calculateExercises(exercisedHoursPerDay, daysTarget));
+} catch (error) {
+    let errorMessage = "Something bad happened."
+    if (error instanceof Error) {
+        errorMessage += " Error: " + error.message;
+    }
+    console.log(errorMessage);
+}
+
+export { };
